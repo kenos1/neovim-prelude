d176da3c1ee093d88c8d31fe78bb9e311f75c360
Kenos <75338427+kenos1@users.noreply.github.com>
2023-08-14 00:52:53 -0400

    Fix tabs in alpha.lua

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -4,8 +4,8 @@ return {
     'goolord/alpha-nvim',
     event = 'VimEnter',
     config = function()
-      local alpha = require'alpha'
-      local dashboard = require'alpha.themes.dashboard'
+      local alpha = require 'alpha'
+      local dashboard = require 'alpha.themes.dashboard'
 
       dashboard.section.header.val = {
         [[                                                  ]],
@@ -19,13 +19,13 @@ return {
         [[╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
       }
       dashboard.section.buttons.val = {
-        dashboard.button("f", " " .. " Find file",    "<cmd>Telescope find_files <cr>"),
-        dashboard.button("n", " " .. " New file",     "<cmd>ene <BAR> startinsert <cr>"),
+        dashboard.button("f", " " .. " Find file", "<cmd>Telescope find_files <cr>"),
+        dashboard.button("n", " " .. " New file", "<cmd>ene <BAR> startinsert <cr>"),
         dashboard.button("r", " " .. " Recent files", "<cmd>Telescope oldfiles <cr>"),
-        dashboard.button("g", " " .. " Find text",    "<cmd>Telescope live_grep <cr>"),
-        dashboard.button("c", " " .. " Config",       "<cmd>e $MYVIMRC <cr>"),
-        dashboard.button("l", "󰒲 " .. " Lazy",         "<cmd>Lazy<cr>"),
-        dashboard.button("q", " " .. " Quit",         "<cmd>qa<cr>"),
+        dashboard.button("g", " " .. " Find text", "<cmd>Telescope live_grep <cr>"),
+        dashboard.button("c", " " .. " Config", "<cmd>e $MYVIMRC <cr>"),
+        dashboard.button("l", "󰒲 " .. " Lazy", "<cmd>Lazy<cr>"),
+        dashboard.button("q", " " .. " Quit", "<cmd>qa<cr>"),
       }
 
       -- Stolen from LazyVim lol
@@ -34,7 +34,8 @@ return {
         callback = function()
           local stats = require("lazy").stats()
           local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-          dashboard.section.footer.val = "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+          dashboard.section.footer.val = "⚡ Neovim loaded " ..
+              stats.count .. " plugins in " .. ms .. "ms"
           pcall(vim.cmd.AlphaRedraw)
         end,
       })

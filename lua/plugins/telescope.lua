204fe04e498d0b9828d00ef718a0da8df1826ba3
Kenos <75338427+kenos1@users.noreply.github.com>
2023-08-14 14:58:14 -0400

    Add optional NvChad theme into telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -25,6 +25,31 @@ return {
     dependencies = {
       'nvim-lua/plenary.nvim',
     },
-    opt = {}
+    opts = {},
+    -- NvChad Telescope theme
+    -- init = function()
+    --   vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+    --     callback = function()
+    --       local normal = vim.api.nvim_get_hl(0, { name = 'Normal', link = false })
+    --       local fg, bg = normal.fg, normal.bg
+    --       local bg_alt = vim.api.nvim_get_hl(0, { name = 'Visual', link = false }).bg
+    --       local primary = vim.api.nvim_get_hl(0, { name = 'Error', link = false }).fg
+    --       local secondary = vim.api.nvim_get_hl(0, { name = 'String', link = false }).fg
+    -- 
+    --       vim.api.nvim_set_hl(0, 'TelescopeBorder', { fg = bg_alt, bg = bg })
+    --       vim.api.nvim_set_hl(0, 'TelescopeNormal', { bg = bg })
+    --       vim.api.nvim_set_hl(0, 'TelescopePreviewBorder', { fg = bg, bg = bg })
+    --       vim.api.nvim_set_hl(0, 'TelescopePreviewNormal', { bg = bg })
+    --       vim.api.nvim_set_hl(0, 'TelescopePreviewTitle', { fg = bg, bg = secondary })
+    --       vim.api.nvim_set_hl(0, 'TelescopePromptBorder', { fg = bg_alt, bg = bg_alt })
+    --       vim.api.nvim_set_hl(0, 'TelescopePromptNormal', { fg = fg, bg = bg_alt })
+    --       vim.api.nvim_set_hl(0, 'TelescopePromptPrefix', { fg = primary, bg = bg_alt })
+    --       vim.api.nvim_set_hl(0, 'TelescopePromptTitle', { fg = bg, bg = primary })
+    --       vim.api.nvim_set_hl(0, 'TelescopeResultsBorder', { fg = bg, bg = bg })
+    --       vim.api.nvim_set_hl(0, 'TelescopeResultsNormal', { bg = bg })
+    --       vim.api.nvim_set_hl(0, 'TelescopeResultsTitle', { fg = bg, bg = bg })
+    --     end,
+    --   })
+    -- end
   }
 }
